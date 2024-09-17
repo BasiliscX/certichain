5f3e1b1bff7c5b4382f47da010fcee1f8d3a53ce
BasiliscX <juniornavarro1988@gmail.com>
2024-09-17 19:54:12 -0300

    Refactor Login page to use consistent button styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,15 +14,17 @@ declare global {
 
 const Login = () => {
   const { ethAccount, connectMetaMask, disconnectMetaMask } = useWeb3();
+  const buttonStyle =
+    "group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white";
 
   return (
     <>
       <Header />
       <motion.div
         className="relative flex items-center justify-center min-h-screen bg-gray-50"
-        initial={{ y: -1000 }} // Posición inicial arriba
-        animate={{ y: 0 }} // Deslizar hacia abajo
-        transition={{ duration: 0.5 }} // Duración de la animación
+        initial={{ y: -1000 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.5 }}
       >
         <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
           <h2 className="text-center text-3xl font-extrabold text-gray-900">
@@ -33,14 +35,14 @@ const Login = () => {
               ethAccount ? (
                 <button
                   onClick={disconnectMetaMask}
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                  className={`${buttonStyle} bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500`}
                 >
                   Disconnect MetaMask
                 </button>
               ) : (
                 <button
                   onClick={connectMetaMask}
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                  className={`${buttonStyle} bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500`}
                 >
                   Connect MetaMask
                 </button>
