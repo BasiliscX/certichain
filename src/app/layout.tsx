d349f8114847a331d7734b5601db9acec7a06bbc
BasiliscX <juniornavarro1988@gmail.com>
2024-09-19 19:40:09 -0300

    Refactor layout and page components to include header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Web3Provider } from "@/context/web3context/Web3Provider";
+import Header from "@/components/header/Header";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
@@ -9,7 +11,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Web3Provider>{children}</Web3Provider>
+        <Web3Provider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </Web3Provider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Header from "@/components/header/Header";
 import Home from "./home/page";
-import Footer from "../components/Footer";
 
 export default function Page() {
   return (
     <>
-      <Header />
       <Home />
-      <Footer />
     </>
   );
 }
